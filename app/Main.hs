c09f1ddf242a37d961c279120281de25390576eb
shokohara <shokohara@users.noreply.github.com>
2017-06-22 13:55:19 +0900

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,31 +2,35 @@
 module Main where
 
 import App
-import Control.Monad
-import Options.Applicative
-import Data.Semigroup ((<>))
-import Option
-import GHC.Int
-import Control.Concurrent
-
-percentOpt :: Parser Float
-percentOpt = option auto (long "percent" <> help "Float")
-
-gbOpt :: Parser Int64
-gbOpt = option auto (long "gb" <> help "Int64")
-
-sample :: Parser Option
-sample = Option <$> gbOpt <*> percentOpt
-
-opts :: ParserInfo Option
-opts = info (sample <**> helper) ( fullDesc
-  <> progDesc "Print a greeting for TARGET"
-    <> header "hello - a test for optparse-applicative" )
+-- import Options.Applicative
+-- import Data.Semigroup ((<>))
+-- import Option
+-- import GHC.Int
+--
+-- percentOpt :: Parser Float
+-- percentOpt = option auto (long "percent" <> help "Float")
+--
+-- gbOpt :: Parser Int64
+-- gbOpt = option auto (long "gb" <> help "Int64")
+--
+-- sample :: Parser Option
+-- sample = Option <$> gbOpt <*> percentOpt
+--
+-- opts :: ParserInfo Option
+-- opts = info (sample <**> helper) ( fullDesc
+--   <> progDesc "Print a greeting for TARGET"
+--       <> header "hello - a test for optparse-applicative" )
+--
+-- main2 :: IO ()
+-- main2 = execParser opts >>= run
+--
+-- main3 :: IO ()
+-- main3 = do
+--   options <- execParser opts
+--   forever $ do
+--     _ <- App.run options
+--     threadDelay (1 * 1000000)
 
 main :: IO ()
-main = do
-  options <- execParser opts
-  forever $ do
-    _ <- App.run options
-    threadDelay (1 * 1000000)
+main = run
 
--- src/App.hs
+++ src/App.hs
@@ -1,101 +1,70 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module App where
 
-import Control.Lens ((&), (.~), (<&>), (?~), (^.))
-import Network.Google
-import Control.Monad.IO.Class  (MonadIO (..))
-import qualified Network.HTTP.Client as Client
-import GHC.Int
-import Network.Google.Compute
-import Control.Monad.Trans.Resource (runResourceT)
-import Data.Aeson
-import Data.Maybe
-import System.IO (stdout, hGetContents)
-import qualified Network.Google as Google
-import qualified Network.Google.Compute as Compute
-import System.Process
-import GHC.Generics
-import Option
-import qualified Option as O
-import Network.HTTP.Client
-import Network.Google.Compute.Metadata
-import Data.Text (Text)
-import qualified Data.Text as T
-import Control.Monad
+import           Control.Monad.Trans.Except
+import           Data.Aeson
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           System.IO
 
-data MetaDisk = MetaDisk { deviceName :: Text } deriving (Show, Generic)
+type ItemApi =
+  "item" :> Get '[JSON] [Item] :<|>
+  "item" :> Capture "itemId" Integer :> Get '[JSON] Item
 
-instance FromJSON MetaDisk
+itemApi :: Proxy ItemApi
+itemApi = Proxy
 
-run :: Option -> IO ()
-run config = storategy (O.percent config) (fromIntegral $ O.gb config) >>= \x ->
-  case x of
-    Just gbR -> do
-      m <- newManager defaultManagerSettings
-      projectId <- exampleProjectId m
-      zoneR <- exampleZone m
-      diskNameM <- listToMaybe <$> exampleListDisks m
-      case diskNameM of
-        Just disks -> do
-          gbM <- exampleGetDisks projectId (deviceName disks) zoneR
-          case gbM of
-            Just gbRR -> void $ exampleDiskResizeGCP projectId (deviceName disks) zoneR (gbR + gbRR)
-            Nothing -> putStrLn "no Disk"
-          exampleDiskResizeOS
-        Nothing -> putStrLn "no Disk"
-    Nothing -> return ()
+-- * app
 
-exampleDiskResizeOS :: IO ()
-exampleDiskResizeOS = do
-  (_, Just parted, _, _) <- createProcess (proc "sudo" ["parted", "/dev/sda", "resizepart", "1", "yes", "100%"]) { std_out = CreatePipe }
-  a <- hGetContents parted
-  print a
-  (_, Just resize2fs, _, _) <- createProcess (proc "sudo" ["resize2fs", "/dev/sda1"]) { std_out = CreatePipe }
-  b <- hGetContents resize2fs
-  print b
+run :: IO ()
+run = do
+  let port = 3000
+      settings =
+        setPort port $
+        setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
+        defaultSettings
+  runSettings settings =<< mkApp
 
-exampleDiskResizeGCP :: Text -> Text -> Text -> GHC.Int.Int64 -> IO Operation
-exampleDiskResizeGCP p d z gbR = do
-  lgr <- Google.newLogger Google.Debug stdout
-  envR <- Google.newEnv <&> (Google.envLogger .~ lgr) . (Google.envScopes .~ Compute.computeScope)
-  runResourceT . Google.runGoogle envR $ Google.send $ disksResize p d z (disksResizeRequest & drrSizeGb ?~ gbR)
+mkApp :: IO Application
+mkApp = return $ serve itemApi server
 
-getDisk :: MonadIO m => Manager -> m [MetaDisk]
-getDisk m = do
-  rs <- getMetadata "instance/disks/?alt=json&recursive=true" [] m
-  case eitherDecode' (Client.responseBody rs) of
-    Left  _  -> fail "fail to decode"
-    Right xs -> pure xs
+server :: Server ItemApi
+server =
+  getItems :<|>
+  getItemById
 
-exampleListDisks :: Manager -> IO [MetaDisk]
-exampleListDisks m = isGCE m >>= \x -> if x then getDisk m else fail "no GCE"
+getItems :: Handler [Item]
+getItems = return [exampleItem]
 
-exampleGetDisks :: Text -> Text -> Text -> IO (Maybe Int64)
-exampleGetDisks p d z = do
-  lgr <- Google.newLogger Google.Debug stdout
-  envR <- Google.newEnv <&> (Google.envLogger .~ lgr) . (Google.envScopes .~ Compute.computeReadOnlyScope)
-  flip (^.) dSizeGb <$> (runResourceT . Google.runGoogle envR $ Google.send $ disksGet p d z)
+getItemById :: Integer -> Handler Item
+getItemById = \ case
+  0 -> return exampleItem
+  _ -> throwE err404
 
-storategy :: Float -> Int64 -> IO (Maybe Int64)
-storategy a b = (\x -> if a < (fromIntegral x * 100) then Just b else Nothing) <$> used
+exampleItem :: Item
+exampleItem = Item 0 "example item"
 
--- return 0-100 percent
-used :: IO Int
-used = do
-  (_, Just df, _, ph1) <- createProcess (proc "df" ["/dev/sda1"]) { std_out = CreatePipe }
-  (_, Just tl, _, ph2) <- createProcess (proc "tail" ["-n", "1"]) { std_out = CreatePipe, std_in = UseHandle df }
-  (_, Just awk, _, ph3) <- createProcess (proc "awk" ["{print $5}"]) { std_out = CreatePipe, std_in = UseHandle tl }
-  c <- hGetContents awk
-  _ <- waitForProcess ph1
-  _ <- waitForProcess ph2
-  _ <- waitForProcess ph3
-  return $ (read :: String -> Int) $ reverse . drop 2 . reverse $ c -- drop "%\n"
+-- * item
 
-exampleProjectId :: Manager -> IO Text
-exampleProjectId m = isGCE m >>= \x -> if x then getProjectId m else fail "no GCE"
+data Item
+  = Item {
+    itemId :: Integer,
+    itemText :: String
+  }
+  deriving (Eq, Show, Generic)
 
-exampleZone :: Manager -> IO Text
-exampleZone m = (T.reverse . T.takeWhile (\x -> x /= '/') . T.reverse) <$> (isGCE m >>= \x -> if x then getZone m else fail "no GCE")
+instance ToJSON Item
+instance FromJSON Item
+
+data a + b = Foo a b
+
+type X = Int + Bool
 
