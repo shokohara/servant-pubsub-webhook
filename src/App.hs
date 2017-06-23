4aec86da9d1c2226c52886bfc6b3418829b04f9e
shokohara <shokohara@users.noreply.github.com>
2017-06-23 17:31:48 +0900

    .

--- src/App.hs
+++ src/App.hs
@@ -13,7 +13,6 @@ import     Control.Monad
 import Data.Either.Combinators
 import           Control.Monad.Trans.Class    (lift)
 import Control.Monad.IO.Class (liftIO)
-import           Control.Monad.Trans.Except
 import           Data.Aeson
 import           GHC.Generics
 import           Network.Wai
@@ -25,42 +24,19 @@ import Network.HTTP.Client hiding (Proxy)
 
 data Val = Val { name :: String, value :: Int } deriving (Show, Generic, FromJSON, ToJSON)
 
-type MyApi = "list" :> Get '[JSON] (Maybe Val)
+type ClientApi = "api" :> Get '[JSON] (Maybe Val)
 
-myApi :: Proxy MyApi
-myApi = Proxy
+type ServerApi = "api" :> Get '[JSON] (Maybe Val)
+--type ServerApi = "api" :> QueryParam "ip" String :> Get '[JSON] (Maybe Val)
+
+clientApi :: Proxy ClientApi
+clientApi = Proxy
+
+serverApi :: Proxy ServerApi
+serverApi = Proxy
 
 getAllBooks :: ClientM (Maybe Val)
-getAllBooks = client myApi
-
-appMain :: IO ()
-appMain = do
-  manager <- newManager defaultManagerSettings
-  res <- runClientM getAllBooks (ClientEnv manager (BaseUrl Http "google.com" 80 ""))
-  case res of
-    Left err -> putStrLn $ "Error: " ++ show err
-    Right books -> print books
-
-itemApi :: Proxy ItemApi
-itemApi = Proxy
-
-run :: Option -> IO ()
-run o = do
-  let settings =
-        setPort (O.port o) $
-          setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ (show $ O.port o))) defaultSettings
-  runSettings settings =<< mkApp
-
-mkApp :: IO Application
-mkApp = return $ serve itemApi server
-
-type ItemApi = "item" :> Get '[JSON] (Maybe Val)
-
-api :: Proxy ItemApi
-api = Proxy
-
-server :: Server ItemApi
-server = lift app3
+getAllBooks = client clientApi
 
 appMain2 :: IO (Either ServantErr (Maybe Val))
 appMain2 = do
@@ -70,27 +46,17 @@ appMain2 = do
 app3 :: IO (Maybe Val)
 app3 = join . rightToMaybe <$> appMain2
 
-getItemById :: Integer -> Handler Item
-getItemById = \ case
-  0 -> return exampleItem
-  _ -> throwE err404
+server :: Server ServerApi
+server = lift app3
 
-exampleItem :: Item
-exampleItem = Item 0 "example item"
+mkApp :: IO Application
+mkApp = return $ serve serverApi server
 
--- * item
+run :: Option -> IO ()
+run o = do
+  let settings =
+        setPort (O.port o) $
+          setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ (show $ O.port o))) defaultSettings
+  runSettings settings =<< mkApp
 
-data Item
-  = Item {
-    itemId :: Integer,
-    itemText :: String
-  }
-  deriving (Eq, Show, Generic)
-
-instance ToJSON Item
-instance FromJSON Item
-
-data a + b = Foo a b
-
-type X = Int + Bool
 
